c68f2eae9a6008aeb1455bdb849f99c27909fee4
Vaibhav Saini <Vaibhavsaini.msrit@gmail.com>
2024-12-06 02:32:00 +0530

    add styling and dialog box for input

--- app.js
+++ app.js
@@ -3,3 +3,22 @@ const myLibrary = [];
 function Book() {}
 
 function addBookToLibrary() {}
+
+const dialog = document.querySelector("dialog");
+const showDialog = document.querySelector("#add-btn");
+
+showDialog.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+dialog.addEventListener("click", (e) => {
+  const dialogDimensions = dialog.getBoundingClientRect();
+  if (
+    e.clientX < dialogDimensions.left ||
+    e.clientX > dialogDimensions.right ||
+    e.clientY < dialogDimensions.top ||
+    e.clientY > dialogDimensions.bottom
+  ) {
+    dialog.close();
+  }
+});
