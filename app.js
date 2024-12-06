3582a71c9eb65f8ececc7db2b9a59b20c5f10485
Vaibhav Saini <Vaibhavsaini.msrit@gmail.com>
2024-12-06 19:34:10 +0530

    add grid style to cards, fix backdrop gradient

--- app.js
+++ app.js
@@ -1,16 +1,88 @@
-const myLibrary = [];
+const myLibrary = [
+  { title: "Book1", author: "Author1", pages: "200", isRead: true },
+  { title: "Book2", author: "Author2", pages: "150", isRead: false },
+];
 
-function Book() {}
+function Book(title, author, pages, isRead) {
+  // the constructor...
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = isRead;
+}
 
-function addBookToLibrary() {}
+// Disable Submit Button (Default Behaviour)
+const submit_btn = document.querySelector("#submit-btn");
+submit_btn.addEventListener(
+  "click",
+  (e) => {
+    e.preventDefault();
+  },
+  false
+);
 
+// To get values
+const input_title = document.querySelector("#input_title");
+const input_author = document.querySelector("#input_author");
+const input_number = document.querySelector("#input_number");
+const input_checkbox = document.querySelector("#read");
+
+// Close Dialog with Submit Button
+submit_btn.addEventListener("click", () => {
+  dialog.close();
+  input_title.value = "";
+  input_author.value = "";
+  input_number.value = "";
+  input_checkbox.checked = false;
+});
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+// LOOP TO ADD myLibrary BOOKS to DOM
+for (let books of myLibrary) {
+  const div_card = document.createElement("div");
+  div_card.classList.add("card");
+
+  const title = document.createElement("h4");
+  const author = document.createElement("p");
+  const pages = document.createElement("p");
+  const buttons = document.createElement("div");
+  buttons.setAttribute("id", "buttons");
+  const button1 = document.createElement("button");
+  const button2 = document.createElement("button");
+  button1.classList.add("button1");
+  button2.classList.add("button2");
+
+  title.textContent = books.title;
+  author.textContent = books.author;
+  pages.textContent = books.pages;
+  button1.textContent = "Not Read";
+  button2.textContent = "Delete";
+
+  div_card.appendChild(title);
+  div_card.appendChild(author);
+  div_card.appendChild(pages);
+  div_card.appendChild(buttons);
+  buttons.appendChild(button1);
+  buttons.appendChild(button2);
+
+  const cards = document.querySelector("#cards");
+  cards.appendChild(div_card);
+}
+
+// Delete button for individual cards
+// button2.addEventListener("click", () => {});
+
+// TO TOGGLE DIALOG BOX FOR INPUT
 const dialog = document.querySelector("dialog");
 const showDialog = document.querySelector("#add-btn");
 
 showDialog.addEventListener("click", () => {
   dialog.showModal();
 });
-
+// TO CLOSE DIALOG BOX BY CLICKING ON BACKDROP
 dialog.addEventListener("click", (e) => {
   const dialogDimensions = dialog.getBoundingClientRect();
   if (
